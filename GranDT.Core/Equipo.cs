8fcb84ff015d181585841de160782f4c07c09016
maaddyy-m <nicoleemartinn712@gmail.com>
2025-10-02 11:32:37 -0300

    Agregadas clases

---
+++ GranDT.Core/Equipo.cs
@@ -0,0 +1,9 @@
+namespace GranDT.Core;
+
+public class Equipo {
+
+    public int idEquipo { get; set; }
+    public byte Cantidad { get; set; }
+    public string Nombre { get; set; }
+    
+}
---
+++ GranDT.Core/FutbolistaPlantilla.cs
@@ -0,0 +1,9 @@
+
+namespace GranDT.Core;
+
+public class FutbolistaPlantilla
+{
+    public int idFutbolista { get; set; }
+
+    public int idPlantilla { get; set; }
+}
