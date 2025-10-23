49b54b4e72890ea80c3f12e0e52337d7094ac47b
maaddyy-m <nicoleemartinn712@gmail.com>
2025-10-23 12:35:22 -0300

    repos nuevos

--- GranDT.Core/Plantilla.cs
+++ GranDT.Core/Plantilla.cs
@@ -1,11 +1,2 @@
-namespace GranDT.Core;
-
-public class Plantilla
-{
-
-    public int idPlantilla { get; set; }
-
-    public string Nombre { get; set; }
-
     public int idUsuario { get; set; }
 }
