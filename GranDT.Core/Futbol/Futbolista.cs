0c05896a7e2d21ede9606c197481f769b1dd918a
Antoniosantacruz <ramosfloresm12de1@gmail.com>
2025-10-29 11:53:52 -0300

    Arreglando strings

--- GranDT.Core/Futbol/Futbolista.cs
+++ GranDT.Core/Futbol/Futbolista.cs
@@ -5,13 +5,13 @@ public class Futbolista
 
     public int idFutbolistas { get; set; }
 
-    public string Nombre { get; set; }
-    public string Apodo { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Apodo { get; set; } = string.Empty;
     public DateTime Nacimiento { get; set; }
     public int idEquipo { get; set; }
     public int idTipoDeJugador { get; set; }
     public decimal Cotizacion { get; set; }
-    public string Creado_por { get; set; }
+    public string Creado_por { get; set; } = string.Empty;
     public int TipoDeJugadorr_idTipoDeJugador { get; set; }
     public int TipoDeJugadorr_idFutbolista { get; set; }
     public int Equipo_idEquipo { get; set; }
